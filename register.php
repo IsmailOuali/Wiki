15de580288621991d96978e5e3e3a2bfd6f95715
unknown <ismail.ouali@ump.ac.ma>
2024-01-10 10:27:38 +0100

    Adding regex not working yet

--- register.php
+++ register.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="js/reg.js"></script>
+
     <title>Registrer a Wiki</title>
 </head>
 <style>
@@ -56,6 +58,9 @@
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-white">Votre email</label>
                             <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                        </div>
+                        <div id="EmailInputHelp" class="form-text text-danger | error_para">
+                            please enter a valid email (exemple@gmail.com).
                         </div>
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-white">Mot de passe</label>
